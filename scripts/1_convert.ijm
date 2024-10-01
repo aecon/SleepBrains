cc4a24587f503f3e542d9404657a2ccc5c65fc61
User <user@hestia-pc.ext.usz.ch>
2024-10-01 11:02:12 +0200

    initial import

---
+++ scripts/1_convert.ijm
@@ -0,0 +1,42 @@
+//input_directory= "/media/user/SSD1/Athena/Data/REUS_VENAD_TREVC_Desiree/";
+input_directory= "/media/user/SSD1/Athena/Data/test/";
+
+directories = getFileList(input_directory);
+Ndir = directories.length;
+
+setBatchMode(true);
+// Loop over sample directories
+for(i=0; i<Ndir; i++){
+	print("Processing directory", i+1, "/", Ndir, ":", directories[i]);
+	
+	// Find channel files inside the directory
+	files = getFileList(input_directory + directories[i]);
+	Nfiles = files.length;
+	print("Number of files:", Nfiles);
+	if (Nfiles != 2){
+		print("Number of files not equal to 2!");	
+		continue;
+	}
+	
+	// Loop over channel files
+	for(j=0; j<Nfiles; j++){
+		print("  ", files[j]);
+		
+		// Load file
+		path = input_directory + directories[i] + files[j];
+		print(path);
+		
+		open(path);
+		title = getTitle();
+		
+		// Save as nrrd
+		output_path = input_directory + directories[i] + "original_" + files[j] + ".nrrd";
+		print(output_path);
+		run("Nrrd ... ", "nrrd=" + output_path);
+		
+		// Close open files
+		run("Close All");
+	}
+}
+print("DONE.");
+setBatchMode(false);
---
+++ scripts/4_alignment/run_elastix_atlas2autof_SLEEP.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -eu
+
+#
+# Registration of atlas onto the autofluorescence channel
+# retult: a transformed atlas file to match the autof. channel
+#
+
+if [ "$#" -ne 2 ]; then
+    echo "run_elastix_atlas2autof.sh requires 2 input arguments:"
+    echo "./run_elastix_atlas2autof.sh <OUTPUT DIRECTORY> <AUTOFLUORESCENCE CHANNEL>"
+    exit
+fi
+
+ELASTIX_PATH=/media/user/SSD1/Athena/SOURCE/elastix-5.2.0-linux
+export LD_LIBRARY_PATH=${ELASTIX_PATH}/lib/
+elastix="${ELASTIX_PATH}/bin/elastix"
+transformix="${ELASTIX_PATH}/bin/transformix"
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+out="$1"
+input_auto="$2"
+echo $out
+echo $input_auto
+
+
+atlas="/media/user/SSD1/Athena/SOURCE/PROJECT_SLEEP_2024-09/alignment/atlas/ABA_25um_reference_hemisphere.nrrd"
+ls $atlas
+atlas_annotation="/media/user/SSD1/Athena/SOURCE/PROJECT_SLEEP_2024-09/alignment/atlas/ABA_25um_annotation_hemisphere.nrrd"
+ls $atlas_annotation
+affine=${SCRIPTPATH}/"affine.txt"
+bspline=${SCRIPTPATH}/"bspline.txt"
+ls $affine
+ls $bspline
+threads=32
+
+outEa=${out}/elastix_affine
+outEb=${out}/elastix_bspline
+outT=${out}/transformix
+mkdir -p "${outEa}"
+mkdir -p "${outEb}"
+mkdir -p "${outT}"
+
+# registration of atlas onto the autofluorescence channel
+if [ ! -f "${outEa}/result.0.nrrd" ]; then
+    ${elastix} -out "${outEa}" -m "${atlas}" -f "${input_auto}" -p "${affine}" -threads $threads
+fi
+if [ ! -f "${outEb}/result.0.nrrd" ]; then
+    ${elastix} -out "${outEb}" -m "${atlas}" -f "${input_auto}" -p "${bspline}" -t0 "${outEa}/TransformParameters.0.txt"  -threads $threads
+fi
+
+# 
+# # transformation of annotation atlas 
+# if [ ! -f "${outT}/result.nrrd" ]; then
+#     cp "${outEb}/TransformParameters.0.txt" "${outT}"/
+#     # edit the Bspline file to use for transforming binary segmented data
+#     sed -i "/FinalBSplineInterpolationOrder/c\(FinalBSplineInterpolationOrder 0)" "${outT}"/TransformParameters.0.txt
+#     # apply transformation to segmentation
+#     ${transformix} -in "${atlas_annotation}" -out "${outT}" -tp "${outT}/TransformParameters.0.txt" -threads $threads
+# fi
+# 
