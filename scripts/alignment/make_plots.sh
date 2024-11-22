afdb44b13ff376aba107f6474844dbd65553571a
Athena Economides <athena.economides@uzh.ch>
2024-11-22 14:43:45 +0100

    make plots

--- scripts/alignment/make_plots.sh
+++ scripts/alignment/make_plots.sh
@@ -2,8 +2,16 @@
 set -eu
 
 parentdir="/media/user/SSD1/Athena/Data/PROJECT_SLEEP_2024-09"
-brains=`find $parentdir/Brain*/align/elastix_bspline -name result.0.nrrd`
-outdir="out"
+
+#brains=`find $parentdir/Brain*/align/elastix_bspline -name result.0.nrrd`
+#brains=`find $parentdir/Brain*/align/elastix_affine -name result.0.nrrd`
+#brains=`find $parentdir/Brain*/align/elastix_bspline_Grid50-300_6000samples_NoBulb -name result.0.nrrd`
+#brains=`find $parentdir/Brain*/align/elastix_bspline_Grid50-300_6000samples -name result.0.nrrd`
+#brains=`find $parentdir/Brain*/align/elastix_bspline_Grid6-2-1_6ksamples_Smooth4-2-1_withBulb_try05 -name result.0.nrrd`
+brains=`find $parentdir/Brain*/align/elastix_bspline_Grid6-2-1_6ksamples_Smooth4-2-1_withBulb_try05 -name result.0.nrrd`
+
+outdir="out_withBulb_try05"
+
 atlas="atlas/ABA_25um_reference_hemisphere.nrrd"
 
 mkdir -p $outdir
