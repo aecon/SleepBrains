d93c6f0b7582a21d8499b778487d48b61deafc8a
Athena Economides <athena.economides@uzh.ch>
2024-11-23 14:16:58 +0100

    export intensity stats in csv file

--- scripts/classification/add_label_intensity.py
+++ scripts/classification/add_label_intensity.py
@@ -87,9 +87,9 @@ raw     = load_nrrd(file_raw, dtype=np.uint16)
 labels3 = load_nrrd(file_labels, dtype=np.float32)  # MLJ output is in float32
 
 # Load csv data
-data = pd.read_csv(file_csv)
-labels      = data["Label"].to_numpy()
-volumes     = data["VoxelCount"].to_numpy()  # In Voxels
+df = pd.read_csv(file_csv)
+labels  = df["Label"].to_numpy()
+volumes = df["VoxelCount"].to_numpy()  # In Voxels
 
 # Compute object intensity stats (mean, std)
 #
@@ -105,17 +105,11 @@ intensity_mean = np.zeros(len(labels))
 intensity_std  = np.zeros(len(labels))
 get_intensity_stats(raw, labels, volumes, coordinates, intensity_mean, intensity_std)
 
+# Add column to dataframe
+df.['IntensityAvg'] = intensity_mean
+df.['IntensityStd'] = intensity_std
 
-
-assert(0)
-
-print("Colouring brain ...")
-assert(len(labels) == np.max(labels))
-# - by volume
-brain_coloured_volume = np.zeros(np.shape(labels3), dtype=np.int32)
-colour(labels3, brain_coloured_volume, labels, volumes)
-# - by sphericity
-brain_coloured_sph = np.zeros(np.shape(labels3), dtype=np.float32)
-colour(labels3, brain_coloured_sph, labels, spericity)
-
+# Export to csv
+output_path = file_csv + "_withIntensity.csv"
+df.to_csv(output_path, index=False)
 
