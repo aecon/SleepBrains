ecfb04fafba0765002a2d1d5518b5e4778e9448e
Athena Economides <athena.economides@uzh.ch>
2024-11-25 13:29:12 +0100

    plots per case

--- scripts/alignment/make_plots_perCase.sh
+++ scripts/alignment/make_plots_perCase.sh
@@ -4,10 +4,10 @@ set -eu
 parentdir="/media/user/SSD1/Athena/Data/PROJECT_SLEEP_2024-09"
 
 # FILES
-brains=`find $parentdir/Brain*/align/elastix_affine_noBulb_try11 -name result.0.nrrd`
+brains=`find $parentdir/Brain*/align/elastix_bspline_10k_125umGrid1_SmoothAll2_noBulb_BsplineInterpolator_try20 -name result.0.nrrd`
 
 # OUTPUT DIRECTORY
-outdir="out_elastix_affine_noBulb_try11"
+outdir="out_elastix_bspline_10k_125umGrid1_SmoothAll2_noBulb_BsplineInterpolator_try20"
 mkdir -p $outdir
 
 # RUN
