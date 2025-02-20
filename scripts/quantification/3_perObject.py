f00657afb96390cb3b696ab75f10eaaa9f36c432
Athena Economides <athena.economides@uzh.ch>
2025-02-20 16:38:28 +0100

    Get region measurements

--- scripts/quantification/3_perObject.py
+++ scripts/quantification/3_perObject.py
@@ -5,16 +5,18 @@ import nrrd
 import numba
 import skimage.io
 import numpy as np
-
+import pandas as pd
 from generate_atlas_masks import brain_regions_include
 
 
 # DESIGNED TO RUN WITH PARALLEL=FALSE.
 @numba.jit(nopython=True, parallel=False, fastmath=True)
-def count_points_in_region(regions, indices, Nregions):
+def count_points_in_region(regions, indices, volumes, avgprob, Nregions):
     nx,ny,nz = regions.shape
     Np = len(indices)
     counts_per_region = np.zeros(Nregions, dtype=np.int64)
+    volumes_per_region = np.zeros(Nregions, dtype=np.int64)
+    total_prob_per_region = np.zeros(Nregions, dtype=np.float32)
     for pi in range(Np):
         i = indices[pi,0]
         j = indices[pi,1]
@@ -23,7 +25,13 @@ def count_points_in_region(regions, indices, Nregions):
             region_id = regions[i,j,k]
             if region_id > 0:  # if not background
                 counts_per_region[region_id-1] += 1
-    return counts_per_region
+                volumes_per_region[region_id-1] += volumes[pi]
+                total_prob_per_region[region_id-1] += avgprob[pi]
+    region_volumes = np.zeros(Nregions)
+    for i in range(Nregions):
+        region_volumes[i] = np.sum(regions==i+1)
+
+    return counts_per_region, volumes_per_region, total_prob_per_region, region_volumes
 
 
 
@@ -59,40 +67,34 @@ if __name__ == "__main__":
     for file_centroids in paths_centroids:
         print("Processing:")
         print(file_centroids)
+        brainID = int(os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(file_centroids)))).split("Brain")[1].split("_")[0])
+        file_measurements = "/media/user/SSD1/Athena/Data/PROJECT_SLEEP_2024-09/ILASTIK/predictions_488_allBrains_ilp-v4/nrrd/coordinates_Brain%02d_488_centroids_with_measurements.csv" % brainID
+        print(file_measurements)
 
-        # Loop over two different kinds of points: allvoxels and centroids
-        kind = "centroids" #["all", "centroids"]
+
+        kind = "centroids"
         file_points =  file_centroids
 
         # Load transformed points
-        ##points = np.loadtxt(file_points, skiprows=2)
         points = load_points(file_points)
-
         Npoints = len(points)
         indices = (points // pixel_size).astype(np.int64)
 
-        # Get number of points per region
-        counts_per_region = count_points_in_region(regions, indices, Nregions)
+        # Load file with per-object measurements
+        df = pd.read_csv(file_measurements)
+        volumes = df["NumberOfVoxels"].to_numpy()  # In Voxels
+        avgprob = df[" AverageProbability"].to_numpy()
+
+        # Get number of points, volumes and associated probabilities
+        counts_per_region, volumes_per_region, total_prob_per_region, region_volumes = count_points_in_region(regions, indices, volumes, avgprob, Nregions)
 
         # Export to file
         basedir = os.path.dirname(os.path.dirname(os.path.dirname(file_points)))
         brainid = os.path.basename(basedir).split("_")[0]
-        output_file = basedir + os.sep + "region_counts_%s" % channel + brainid + "_%s.txt" % kind
+        output_file = basedir + os.sep + "Brain%02d_region_counts_volumes_probabilities_%s" % (brainID, channel) + brainid + "_%s.txt" % kind
         print(output_file)
         with open(output_file, "w") as f:
+            f.write("%s %s %s %s\n" % ("Region", "ObjCounts" "ObjVolumes" "ObjProbability" "RegionVolume") )
             for i in range(Nregions):
-                f.write("%s %d\n" % (brain_regions_include[i], counts_per_region[i]))
-
-        # Bin transformed points on atlas space
-        nx,ny,nz = regions.shape
-        grid = np.zeros(np.shape(regions), dtype=np.float32)
-        for p in range(Npoints):
-            ix = indices[p,0]
-            iy = indices[p,1]
-            iz = indices[p,2]
-
-            if ((ix<nx) and (iy<ny) and (iz<nz)):
-                grid[ix,iy,iz] += 1
-
-        nrrd.write("%s/aligned_points_on_grid_%s_2025-02_%s_%s.nrrd" % (basedir, channel, brainid, kind), grid)
+                f.write("%s %d\n" % (brain_regions_include[i], counts_per_region[i], volumes_per_region[i], total_prob_per_region[i], region_volumes[i]))
 
--- scripts/quantification/README.md
+++ scripts/quantification/README.md
@@ -7,11 +7,30 @@ Collect region-wise statistics.
 python generate_atlas_masks.py
 ```
 
+* Copy the generated file: `selected_atlas_areas.nrrd` inside the folder:
+```
+scripts/alignment/selected_atlas_areas_pixel25um_noBulb_2025-02.nrrd
+```
+* Then open this file in ImageJ, and also open the file `selected_atlas_areas_pixel25um_noBulb.nrrd`.
+* Crop the new file to be the same size as the `_noBulb` file.
+* Edit the Pixel Properties to be 25 microns.
+* Save the file.
+
+
 2. Generate text files with counts (and volumes) per region, and map pixels to 3D atlas grid
+* Edit the path to the selected atlas regions in the variable `file_atlas`.
+* Edit the path to the transformed points files: `paths_centroids`.
+* Then run:
 ```
-python 2_regional_counts_volumes.py
-python 2_regional_counts_volumes.py
+python 2_regional_counts_volumes_2025-02.py
 ```
+
+**OR**
+Use the following to get counts, volumes, total probability, and region volume
+```
+python 3_perObject.py
+```
+
 
 3. Plotting
 ```
